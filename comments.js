2073c4d201195f4e278265e77e553be60751c851
duodec1m <47816715+duodec1m@users.noreply.github.com>
2019-07-25 22:34:12 -0700

    Fourth Commit
    Managed to get comments for every article to show but duplicate comments show and button must be pressed at least 2 times for the comments section to pop out.

--- comments.js
+++ comments.js
@@ -80,4 +80,32 @@ fetch('http://jsonplaceholder.typicode.com/comments?postId=' + n)
   .catch(function(err) {
     console.log('Fetch Error :-S', err);
   });
+
+(function (window) {
+  'use strict';
+
+  const BUTTON_SELECTOR = '[data-posts="id"]';
+
+  let buttons = document.querySelectorAll(BUTTON_SELECTOR);
+
+  buttons.forEach(function (button) {
+    'use strict';
+
+    let sectionSelector = `#comments-${button.value}`;
+    let commentSection = document.querySelector(sectionSelector);
+
+    button.addEventListener('click', function (event) {
+      if (commentSection.hidden) {
+        commentSection.hidden = false;
+        button.textContent = 'Hide comments';
+      } else {
+        commentSection.hidden = true;
+        button.textContent = 'Show comments';
+      }
+
+      event.preventDefault();
+    });
+  });
+})(window);	
+	
 }
